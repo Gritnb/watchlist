3bd7b62b8cd2cb8bc27e091b49c6698795326c8e
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-09-07 18:24:03 +0300

    Changed incorrect URL

--- components/GitLink.jsx
+++ components/GitLink.jsx
@@ -4,7 +4,7 @@ export default function GitLink() {
     return (
         <section className='git-sct'>
             <p className='author'>© {new Date().getFullYear()} GCarlott</p>
-            <a href='https://github.com/Gritnb/the-watchlist' target='_blank'>
+            <a href='https://github.com/Gritnb/watchlist' target='_blank'>
             <img src={gitLogo}
                  alt='Github Logo'
                  className='git-logo'/>
